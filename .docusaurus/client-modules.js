8943440cb0909e0ed34b04e32f17e3e618b9c36c
Josemalyson Oliveira <josemalyson@gmail.com>
2024-03-13 19:40:27 -0300

    Deploy website - based on 934cc0af44b4174f13ba90ddcd3c199149939415

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/josemalyson/Projects/josemalyson.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/home/josemalyson/Projects/josemalyson.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/josemalyson/Projects/josemalyson.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/josemalyson/Projects/josemalyson.github.io/src/css/custom.css"),
-];
